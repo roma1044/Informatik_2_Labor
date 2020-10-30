42e1c3a08176a89efd81e0061620b2aa3da31b77
Ma. Ro <manurot12@gmail.com>
2020-10-30 17:24:20 +0100

    V1_1

--- Aufgabe_2_C_Punkt/CRechteck.h
+++ Aufgabe_2_C_Punkt/CRechteck.h
@@ -1,4 +1,18 @@
 // Autor: Manuel Rothas
 //Datum: 29.10.2020
-
 #pragma once
+#include "CPunkt.h"
+class CRechteck
+{
+public:
+	CRechteck(const CPunkt& A, const CPunkt& B);
+	double getFlaeche();
+	~CRechteck();
+	double getXmin();
+	double getYmin();
+	double getXmax();
+	double getYmax();
+private:
+	CPunkt mA, mB;
+};
+
